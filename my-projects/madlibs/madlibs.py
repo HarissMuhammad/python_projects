600dfb38727096264957edf1bf51affbe41fb7f9
HarrisMuhammad <haris928762@gmail.com>
2025-03-06 12:57:25 +0500

    Added madlibs projects

---
+++ my-projects/madlibs/madlibs.py
@@ -0,0 +1,20 @@
+adj = input("Adjective : ")
+verb = input("Verb : ")
+city = input("City :")
+sports = input("Football or Cricket")
+verb1 = input("Verb : ")
+verb2 = input("Verb : ")
+team = input("Barca or Madrid : ")
+language = input("Your lanuage :")
+emotion = input("Emotion: ")
+
+
+madlib =f"One day, in the {city}, a {adj} crowd gathered to watch the biggest {sports} match of the year.\
+        The stadium was buzzing with excitement as fans {verb} in anticipation. As the game began,\
+        the players showed their skills, making the audience {verb1} in awe.\
+        The match was between {team}, known for their incredible gameplay. \
+        The commentators, speaking in {language}, described every moment with enthusiasm. \
+        Suddenly, a last-minute goal made the entire stadium erupt with {emotion}.\
+        It was a game that no one would ever forget!"
+
+print(madlib)
---
+++ my-projects/madlibs/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from madlibs!")
+
+
+if __name__ == "__main__":
+    main()
