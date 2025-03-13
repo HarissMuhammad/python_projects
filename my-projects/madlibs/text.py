5b9eaa8e3087b0d4917777a27cade4878f292912
HarrisMuhammad <haris928762@gmail.com>
2025-03-13 11:59:33 +0500

    Added code

---
+++ my-projects/madlibs/text.py
@@ -0,0 +1,16 @@
+city = ""
+adj= ""
+sports = ""
+verb = ""
+verb1 = ""
+team = ""
+language = ""
+emotion = ""
+
+madlib =f"One day, in the {city}, a {adj} crowd gathered to watch the biggest {sports} match of the year.\
+        The stadium was buzzing with excitement as fans {verb} in anticipation. As the game began,\
+        the players showed their skills, making the audience {verb1} in awe.\
+        The match was between {team}, known for their incredible gameplay. \
+        The commentators, speaking in {language}, described every moment with enthusiasm. \
+        Suddenly, a last-minute goal made the entire stadium erupt with {emotion}.\
+        It was a game that no one would ever forget!"
